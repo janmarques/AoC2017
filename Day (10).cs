612c944768bcb689a5d164c39ec0da8a8719ccd6
Jan Marques <jan@janmarques.net>
2025-01-03 16:56:11 +0100

    d10p2

--- Day (10).cs
+++ Day (10).cs
@@ -1,8 +1,10 @@
-﻿var fullInput =
+﻿using System.Text;
+
+var fullInput =
 @"34,88,2,222,254,93,150,0,199,255,39,32,137,136,1,167";
 
 var smallInput =
-@"3,4,1,5";
+@"1,2,3";
 
 var smallest = "";
 
@@ -13,36 +15,58 @@ var timer = System.Diagnostics.Stopwatch.StartNew();
 
 var result = 0;
 
-var list = Enumerable.Range(0, 256).ToList();
-if (input.Length < 20)
-{
-    list = Enumerable.Range(0, 5).ToList();
-}
 
-var lengths = input.Split(",").Select(int.Parse).ToList();
+var list = Enumerable.Range(0, 256).ToList();
+//if (input.Length < 20)
+//{
+//    list = Enumerable.Range(0, 5).ToList();
+//}
+
+var lengths = ASCIIEncoding.ASCII.GetBytes(input).Select(x => (int)x).ToList();
+lengths.Add(17);
+lengths.Add(31);
+lengths.Add(73);
+lengths.Add(47);
+lengths.Add(23);
 
 var pos = 0;
 var skip = 0;
-foreach (var length in lengths)
+for (int j = 0; j < 64; j++)
 {
-    var take = new List<int>();
-    for (int i = 0; i < length; i++)
+    foreach (var length in lengths)
     {
-        take.Add(list[(pos + i) % list.Count]);
+        var take = new List<int>();
+        for (int i = 0; i < length; i++)
+        {
+            take.Add(list[(pos + i) % list.Count]);
+        }
+
+        take.Reverse();
+
+        for (int i = 0; i < length; i++)
+        {
+            list[(pos + i) % list.Count] = take[i];
+        }
+
+        pos += (length + skip) % list.Count;
+        skip++;
     }
-
-    take.Reverse();
-
-    for (int i = 0; i < length; i++)
-    {
-        list[(pos + i) % list.Count] = take[i];
-    }
-
-    pos += (length+skip) % list.Count;
-    skip++;
 }
 
-result = list[0] * list[1];
+var reses = new List<int>();
+for (int i = 0; i < 16; i++)
+{
+    var span = list.Skip(i * 16).Take(16);
+    var res = span.First();
+    foreach (var item in span.Skip(1))
+    {
+        res ^= item;
+    }
+    reses.Add(res);
+}
+
+var str = Convert.ToHexStringLower(reses.Select(x => (byte)x).ToArray());
+Console.WriteLine(str);
 
 timer.Stop();
 Console.WriteLine(result);
