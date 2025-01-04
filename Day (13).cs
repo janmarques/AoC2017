106672b6650676470267d6b230e15195be532a31
Jan Marques <jan@janmarques.net>
2025-01-04 21:48:43 +0100

    d13p1

--- Day (13).cs
+++ Day (13).cs
@@ -1,21 +1,76 @@
-﻿//var fullInput =
-//@"";
+﻿var fullInput =
+@"0: 3
+1: 2
+2: 4
+4: 4
+6: 5
+8: 6
+10: 8
+12: 8
+14: 6
+16: 6
+18: 9
+20: 8
+22: 6
+24: 10
+26: 12
+28: 8
+30: 8
+32: 14
+34: 12
+36: 8
+38: 12
+40: 12
+42: 12
+44: 12
+46: 12
+48: 14
+50: 12
+52: 12
+54: 10
+56: 14
+58: 12
+60: 14
+62: 14
+64: 14
+66: 14
+68: 14
+70: 14
+72: 14
+74: 20
+78: 14
+80: 14
+90: 17
+96: 18";
 
-//var smallInput =
-//@"";
+var smallInput =
+@"0: 3
+1: 2
+4: 4
+6: 4";
 
-//var smallest = "";
+var smallest = "";
 
-//var input = smallInput;
-////var input = fullInput;
-////var input = smallest;
+var input = smallInput;
+input = fullInput;
+//input = smallest;
+var timer = System.Diagnostics.Stopwatch.StartNew();
 
-//var result = "";
+var result = 0;
 
-//foreach (var line in input.Split(Environment.NewLine))
-//{
+var layers = input.Split(Environment.NewLine).Select(x => x.Split(": ").Select(int.Parse)).Select(x => (depth: x.First(), range: x.Last())).ToList();
 
-//}
+for (int i = 0; i <= layers.Max(x => x.depth); i++)
+{
+    var (depth, range) = layers.SingleOrDefault(x => x.depth == i);
+    if (range == 0) { continue; }
+    if (i % (2 * range - 2) == 0)
+    {
+        result += range * depth;
+    }
+}
 
-//Console.WriteLine(result);
-//Console.ReadLine();
+timer.Stop();
+Console.WriteLine(result);
+Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+Console.ReadLine();
