5ade04270ea58fbba6bf9c6ee7f6a8adfcb0fea8
Jan Marques <jan@janmarques.net>
2025-01-05 19:36:09 +0100

    s

--- Day (17).cs
+++ Day (17).cs
@@ -1,21 +1,43 @@
-﻿//var fullInput =
-//@"";
+﻿using AoC2024;
 
-//var smallInput =
-//@"";
+var fullInput = 380;
 
-//var smallest = "";
+var smallInput = 3;
 
-//var input = smallInput;
-////var input = fullInput;
-////var input = smallest;
+var smallest = "";
 
-//var result = "";
+var input = smallInput;
+input = fullInput;
+//input = smallest;
+var timer = System.Diagnostics.Stopwatch.StartNew();
 
-//foreach (var line in input.Split(Environment.NewLine))
-//{
+var result = 0;
 
-//}
+var steps = input;
 
-//Console.WriteLine(result);
-//Console.ReadLine();
+var pos = 0;
+var lst = new List<int>(50000000) { 0 };
+var hashset = new HashSet<int>();
+for (int i = 0; i < 50000000; i++)
+{
+    //Utils.Counter("i", expectedTotal: 50000000, timer: true);
+    pos = (pos + steps) % lst.Count;
+    lst.Insert(pos + 1, i + 1);
+    pos++;
+    if (hashset.Add(lst[1]))
+    {
+        Console.WriteLine($"{i} {lst[1]}");
+    }
+    if (lst[1] != 665)
+    {
+
+    }
+}
+
+
+result = lst[lst.IndexOf(0) + 1];
+
+timer.Stop();
+Console.WriteLine(result);
+Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+Console.ReadLine();
