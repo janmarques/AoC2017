f505c7be2efeea1f42fff2f33e5306e6bab1c39b
Jan Marques <jan@janmarques.net>
2025-01-05 16:32:09 +0100

    d16p1

--- Day (15).cs
+++ Day (15).cs
@@ -1,75 +1,75 @@
-﻿var fullInput =
-@"";
+﻿//var fullInput =
+//@"";
 
-var smallInput =
-@"";
+//var smallInput =
+//@"";
 
-var smallest = "";
+//var smallest = "";
 
-var input = smallInput;
-//input = fullInput;
-//input = smallest;
+//var input = smallInput;
+////input = fullInput;
+////input = smallest;
 
-var aFactor = 16807L;
-var bFactor = 48271L;
+//var aFactor = 16807L;
+//var bFactor = 48271L;
 
-var a = 65L;
-var b = 8921L;
+//var a = 65L;
+//var b = 8921L;
 
-a = 703;
-b = 516;
-
-
-var max = 2147483647;
-
-var timer = System.Diagnostics.Stopwatch.StartNew();
-
-var result = 0;
-
-IEnumerable<long> GetAs()
-{
-    while (true)
-    {
-
-        a = (a * aFactor) % max;
-        if (a % 4 == 0)
-        {
-            yield return a;
-        }
-    }
-}
-
-IEnumerable<long> GetBs()
-{
-    while (true)
-    {
-        b = (b * bFactor) % max;
-        if (b % 8 == 0)
-        {
-            yield return b;
-        }
-    }
-}
-
-var aS = GetAs().Take(5_000_000).ToArray();
-var bS = GetBs().Take(5_000_000).ToArray();
-
-for (var i = 0; i < 5_000_000; i++)
-{
-    var la = aS.ElementAt(i);
-    var lb = bS.ElementAt(i);
-
-    var aBytes = BitConverter.GetBytes(la).Take(2);
-    var bBytes = BitConverter.GetBytes(lb).Take(2);
-
-    if (aBytes.SequenceEqual(bBytes))
-    {
-        result++;
-    }
-}
+//a = 703;
+//b = 516;
 
 
-timer.Stop();
-Console.WriteLine(result);
-Console.WriteLine(timer.ElapsedMilliseconds + "ms");
-Console.ReadLine();
+//var max = 2147483647;
+
+//var timer = System.Diagnostics.Stopwatch.StartNew();
+
+//var result = 0;
+
+//IEnumerable<long> GetAs()
+//{
+//    while (true)
+//    {
+
+//        a = (a * aFactor) % max;
+//        if (a % 4 == 0)
+//        {
+//            yield return a;
+//        }
+//    }
+//}
+
+//IEnumerable<long> GetBs()
+//{
+//    while (true)
+//    {
+//        b = (b * bFactor) % max;
+//        if (b % 8 == 0)
+//        {
+//            yield return b;
+//        }
+//    }
+//}
+
+//var aS = GetAs().Take(5_000_000).ToArray();
+//var bS = GetBs().Take(5_000_000).ToArray();
+
+//for (var i = 0; i < 5_000_000; i++)
+//{
+//    var la = aS.ElementAt(i);
+//    var lb = bS.ElementAt(i);
+
+//    var aBytes = BitConverter.GetBytes(la).Take(2);
+//    var bBytes = BitConverter.GetBytes(lb).Take(2);
+
+//    if (aBytes.SequenceEqual(bBytes))
+//    {
+//        result++;
+//    }
+//}
+
+
+//timer.Stop();
+//Console.WriteLine(result);
+//Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+//Console.ReadLine();
