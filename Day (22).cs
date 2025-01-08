3c77707548ec519c6740431d437d87bec46e39e0
Jan Marques <jan@janmarques.net>
2025-01-08 09:18:22 +0100

    d23p2

--- Day (22).cs
+++ Day (22).cs
@@ -1,122 +1,122 @@
-﻿using AoC2024;
+﻿//using AoC2024;
 
-var fullInput =
-@"...#.#.####.....#.##..###
-##.#.###..#.....#.##...#.
-..#.##..#.#.##.#...#..###
-###...##....###.#..#...#.
-...#..#.........##..###..
-#..#.#.#.#.#.#.#.##.####.
-#...#.##...###...##..#..#
-##...#.###..###...####.##
-###..#.#####.##..###.#.##
-#..#....#.##..####...####
-...#.#......###.#..#..##.
-.#.#...##.#.#####..###.#.
-.....#..##..##..###....##
-#.#..###.##.##.#####.##..
-###..#..###.##.#..#.##.##
-.#######.###....######.##
-..#.#.###.##.##...###.#..
-#..#.####...###..###..###
-#...#..###.##..##...#.#..
-........###..#.#.##..##..
-.#############.#.###..###
-##..#.###....#.#..#..##.#
-..#.#.#####....#..#####..
-.#.#..#...#...##.#..#....
-##.#..#..##........#..##.";
+//var fullInput =
+//@"...#.#.####.....#.##..###
+//##.#.###..#.....#.##...#.
+//..#.##..#.#.##.#...#..###
+//###...##....###.#..#...#.
+//...#..#.........##..###..
+//#..#.#.#.#.#.#.#.##.####.
+//#...#.##...###...##..#..#
+//##...#.###..###...####.##
+//###..#.#####.##..###.#.##
+//#..#....#.##..####...####
+//...#.#......###.#..#..##.
+//.#.#...##.#.#####..###.#.
+//.....#..##..##..###....##
+//#.#..###.##.##.#####.##..
+//###..#..###.##.#..#.##.##
+//.#######.###....######.##
+//..#.#.###.##.##...###.#..
+//#..#.####...###..###..###
+//#...#..###.##..##...#.#..
+//........###..#.#.##..##..
+//.#############.#.###..###
+//##..#.###....#.#..#..##.#
+//..#.#.#####....#..#####..
+//.#.#..#...#...##.#..#....
+//##.#..#..##........#..##.";
 
-var smallInput =
-@"..#
-#..
-...";
+//var smallInput =
+//@"..#
+//#..
+//...";
 
-var smallest = "";
+//var smallest = "";
 
-var input = smallInput;
-input = fullInput;
-//input = smallest;
-var timer = System.Diagnostics.Stopwatch.StartNew();
+//var input = smallInput;
+//input = fullInput;
+////input = smallest;
+//var timer = System.Diagnostics.Stopwatch.StartNew();
 
-var result = 0;
+//var result = 0;
 
-var grid = Utils.ParseCoordGrid(input, x => new Cell { X = x.x, Y = x.y, State = x.c == '#' ? State.Infected : State.Clean }).ToDictionary(x => (x.X, x.Y));
+//var grid = Utils.ParseCoordGrid(input, x => new Cell { X = x.x, Y = x.y, State = x.c == '#' ? State.Infected : State.Clean }).ToDictionary(x => (x.X, x.Y));
 
-var x = (int)Math.Sqrt(grid.Count) / 2;
-var y = x;
-var facing = 'N';
+//var x = (int)Math.Sqrt(grid.Count) / 2;
+//var y = x;
+//var facing = 'N';
 
-string EnumPr(State state)
-{
-    return state switch
-    {
-        State.Clean => ".",
-        State.Weakened => "W",
-        State.Infected => "#",
-        State.Flagged => "F",
-        _ => throw new Exception(),
-    };
-}
-void Print() => Utils.PrintGrid(grid.Values, x => x.X, x => x.Y, x => EnumPr(x.State), nullPrint: (_, _) => ".");
+//string EnumPr(State state)
+//{
+//    return state switch
+//    {
+//        State.Clean => ".",
+//        State.Weakened => "W",
+//        State.Infected => "#",
+//        State.Flagged => "F",
+//        _ => throw new Exception(),
+//    };
+//}
+//void Print() => Utils.PrintGrid(grid.Values, x => x.X, x => x.Y, x => EnumPr(x.State), nullPrint: (_, _) => ".");
 
-for (var i = 0; i < 10000000; i++)
-{
-    //Utils.Counter("d", expectedTotal: 10000000, timer: true);
-    var cell = GetOrCreate();
-    switch (cell.State)
-    {
-        case State.Clean:
-            facing = Utils.RotateLeft(facing);
-            cell.State = State.Weakened;
-            break;
-        case State.Weakened:
-            cell.State = State.Infected;
-            result++;
-            break;
-        case State.Infected:
-            facing = Utils.RotateRight(facing);
-            cell.State = State.Flagged;
-            break;
-        case State.Flagged:
-            facing = Utils.InverseDirection(facing);
-            cell.State = State.Clean;
-            break;
-        default: throw new Exception();
-    }
+//for (var i = 0; i < 10000000; i++)
+//{
+//    //Utils.Counter("d", expectedTotal: 10000000, timer: true);
+//    var cell = GetOrCreate();
+//    switch (cell.State)
+//    {
+//        case State.Clean:
+//            facing = Utils.RotateLeft(facing);
+//            cell.State = State.Weakened;
+//            break;
+//        case State.Weakened:
+//            cell.State = State.Infected;
+//            result++;
+//            break;
+//        case State.Infected:
+//            facing = Utils.RotateRight(facing);
+//            cell.State = State.Flagged;
+//            break;
+//        case State.Flagged:
+//            facing = Utils.InverseDirection(facing);
+//            cell.State = State.Clean;
+//            break;
+//        default: throw new Exception();
+//    }
 
-    var dir = Utils.Directions.Single(x => x.icon == facing);
-    x += dir.x;
-    y += dir.y;
-
-
-    //Print();
-}
-
-Cell GetOrCreate()
-{
-    if (!grid.TryGetValue((x, y), out var value))
-    {
-        value = new Cell { X = x, Y = y, State = State.Clean };
-        grid[(x, y)] = value;
-    }
-    return value;
-}
+//    var dir = Utils.Directions.Single(x => x.icon == facing);
+//    x += dir.x;
+//    y += dir.y;
 
 
-timer.Stop();
-Console.WriteLine(result);
-Console.WriteLine(timer.ElapsedMilliseconds + "ms");
-Console.ReadLine();
+//    //Print();
+//}
 
-class Cell
-{
-    public int X { get; set; }
-    public int Y { get; set; }
-    public State State { get; set; }
-}
+//Cell GetOrCreate()
+//{
+//    if (!grid.TryGetValue((x, y), out var value))
+//    {
+//        value = new Cell { X = x, Y = y, State = State.Clean };
+//        grid[(x, y)] = value;
+//    }
+//    return value;
+//}
 
-enum State
-{
-    Clean, Weakened, Infected, Flagged
-}
+
+//timer.Stop();
+//Console.WriteLine(result);
+//Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+//Console.ReadLine();
+
+//class Cell
+//{
+//    public int X { get; set; }
+//    public int Y { get; set; }
+//    public State State { get; set; }
+//}
+
+//enum State
+//{
+//    Clean, Weakened, Infected, Flagged
+//}
--- Day (23).cs
+++ Day (23).cs
@@ -1,21 +1,132 @@
-﻿//var fullInput =
-//@"";
+﻿using AoC2024;
+using System.Collections.Concurrent;
+using System.Runtime.ExceptionServices;
+using System.Security.AccessControl;
 
-//var smallInput =
-//@"";
+var fullInput =
+@"set b 79
+set c b
+jnz a 2
+jnz 1 5
+mul b 100
+sub b -100000
+set c b
+sub c -17000
+set f 1
+set d 2
+set e 2
+set g d
+mul g e
+sub g b
+jnz g 2
+set f 0
+sub e -1
+set g e
+sub g b
+jnz g -8
+sub d -1
+set g d
+sub g b
+jnz g -13
+jnz f 2
+sub h -1
+set g b
+sub g c
+jnz g 2
+jnz 1 3
+sub b -17
+jnz 1 -23";
 
-//var smallest = "";
+var smallInput =
+@"";
 
-//var input = smallInput;
-////var input = fullInput;
-////var input = smallest;
+var smallest = "";
 
-//var result = "";
+var input = smallInput;
+input = fullInput;
+//input = smallest;
+var timer = System.Diagnostics.Stopwatch.StartNew();
+var result = 0L;
 
-//foreach (var line in input.Split(Environment.NewLine))
-//{
+var a = new Machine(input, 0);
+var b = new Machine(input, 1);
+a.Out = b.In;
+b.Out = a.In;
 
-//}
+a.Execute();
 
-//Console.WriteLine(result);
-//Console.ReadLine();
+//Task.Run(() => a.Execute());
+//Task.Run(() => b.Execute());
+
+timer.Stop();
+Console.WriteLine(result);
+Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+Console.ReadLine();
+
+
+class Machine(string input, int processId)
+{
+    public int MulCnt { get; set; }
+    public BlockingCollection<long> Out { get; set; }
+    public BlockingCollection<long> In { get; set; } = new BlockingCollection<long> { };
+
+    public void Execute()
+    {
+        var registers = new Dictionary<char, long>()
+        {
+            { 'p', processId }
+        };
+
+        var lines = input.Split(Environment.NewLine).ToArray();
+        for (long i = 0; i < lines.Length; i++)
+        {
+            Utils.Counter(processId.ToString(), 1_000_000);
+            var line = lines[i];
+            var split = line.Split(" ").ToArray();
+            var register = split[1].Single();
+            var isRegister = char.IsLetter(register);
+            var bValue = long.MinValue;
+            if (split.Length > 2)
+            {
+                if (char.IsLetter(split[2], 0))
+                {
+                    registers.TryGetValue(split[2].Single(), out bValue);
+                }
+                else
+                {
+                    bValue = long.Parse(split[2]);
+                }
+
+            }
+            if (isRegister && !registers.ContainsKey(register))
+            {
+                registers[register] = 0;
+            }
+            var aValue = isRegister ? registers[register] : long.Parse(register.ToString());
+
+
+            switch (split[0])
+            {
+                case "set":
+                    registers[register] = bValue;
+                    break;
+                case "sub":
+                    registers[register] -= bValue;
+                    break;
+                case "mul":
+                    registers[register] *= bValue;
+                    MulCnt++;
+                    Console.WriteLine($"Program {processId} sending {register} {MulCnt}");
+                    break;
+                case "jnz":
+                    if (aValue != 0)
+                    {
+                        i += bValue - 1;
+                    }
+                    break;
+
+                default: throw new Exception();
+            }
+        }
+    }
+}
