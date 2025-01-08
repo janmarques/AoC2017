36d280b456a2cd9d57bc5f6f69f842773070edbe
Jan Marques <jan@janmarques.net>
2025-01-08 12:43:31 +0100

    d24p1

--- Day (23).cs
+++ Day (23).cs
@@ -1,91 +1,91 @@
-﻿using AoC2024;
-using System.Collections.Concurrent;
-using System.Runtime.ExceptionServices;
-using System.Security.AccessControl;
+﻿//using AoC2024;
+//using System.Collections.Concurrent;
+//using System.Runtime.ExceptionServices;
+//using System.Security.AccessControl;
 
-var fullInput =
-@"set b 79
-set c b
-jnz a 2
-jnz 1 5
-mul b 100
-sub b -100000
-set c b
-sub c -17000
-set f 1
-set d 2
-set e 2
-set g d
-mul g e
-sub g b
-jnz g 2
-set f 0
-sub e -1
-set g e
-sub g b
-jnz g -8
-sub d -1
-set g d
-sub g b
-jnz g -13
-jnz f 2
-sub h -1
-set g b
-sub g c
-jnz g 2
-jnz 1 3
-sub b -17
-jnz 1 -23";
+//var fullInput =
+//@"set b 79
+//set c b
+//jnz a 2
+//jnz 1 5
+//mul b 100
+//sub b -100000
+//set c b
+//sub c -17000
+//set f 1
+//set d 2
+//set e 2
+//set g d
+//mul g e
+//sub g b
+//jnz g 2
+//set f 0
+//sub e -1
+//set g e
+//sub g b
+//jnz g -8
+//sub d -1
+//set g d
+//sub g b
+//jnz g -13
+//jnz f 2
+//sub h -1
+//set g b
+//sub g c
+//jnz g 2
+//jnz 1 3
+//sub b -17
+//jnz 1 -23";
 
-var smallInput =
-@"";
+//var smallInput =
+//@"";
 
-var smallest = "";
+//var smallest = "";
 
-var input = smallInput;
-input = fullInput;
-//input = smallest;
-var timer = System.Diagnostics.Stopwatch.StartNew();
-var result = 0L;
+//var input = smallInput;
+//input = fullInput;
+////input = smallest;
+//var timer = System.Diagnostics.Stopwatch.StartNew();
+//var result = 0L;
 
-result = Exec();
+//result = Exec();
 
-int Exec()
-{
-    int b = 0, c = 0, h = 0;
+//int Exec()
+//{
+//    int b = 0, c = 0, h = 0;
 
-    b = 79;
-    c = b;
+//    b = 79;
+//    c = b;
 
-    b *= 100;
-    b += 100000;
-    c = b;
-    c += 17000;
-
-
-    for (; b - 17 < c; b = b + 17)
-    {
-        if (!IsPrime(b))
-        {
-            h++;
-        }
-    }
-    return h;
-}
+//    b *= 100;
+//    b += 100000;
+//    c = b;
+//    c += 17000;
 
 
-timer.Stop();
-Console.WriteLine(result); //1001 too high
-Console.WriteLine(timer.ElapsedMilliseconds + "ms");
-Console.ReadLine();
+//    for (; b - 17 < c; b = b + 17)
+//    {
+//        if (!IsPrime(b))
+//        {
+//            h++;
+//        }
+//    }
+//    return h;
+//}
 
-bool IsPrime(int n)
-{
-    if (n > 1)
-    {
-        return Enumerable.Range(1, n).Where(x => n % x == 0)
-                         .SequenceEqual(new[] { 1, n });
-    }
 
-    return false;
-}
+//timer.Stop();
+//Console.WriteLine(result); //1001 too high
+//Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+//Console.ReadLine();
+
+//bool IsPrime(int n)
+//{
+//    if (n > 1)
+//    {
+//        return Enumerable.Range(1, n).Where(x => n % x == 0)
+//                         .SequenceEqual(new[] { 1, n });
+//    }
+
+//    return false;
+//}
--- Day (24).cs
+++ Day (24).cs
@@ -1,21 +1,114 @@
-﻿//var fullInput =
-//@"";
+﻿using System.Numerics;
+using System.Xml.Linq;
 
-//var smallInput =
-//@"";
+var fullInput =
+@"48/5
+25/10
+35/49
+34/41
+35/35
+47/35
+34/46
+47/23
+28/8
+27/21
+40/11
+22/50
+48/42
+38/17
+50/33
+13/13
+22/33
+17/29
+50/0
+20/47
+28/0
+42/4
+46/22
+19/35
+17/22
+33/37
+47/7
+35/20
+8/36
+24/34
+6/7
+7/43
+45/37
+21/31
+37/26
+16/5
+11/14
+7/23
+2/23
+3/25
+20/20
+18/20
+19/34
+25/46
+41/24
+0/33
+3/7
+49/38
+47/22
+44/15
+24/21
+10/35
+6/21
+14/50";
 
-//var smallest = "";
+var smallInput =
+@"0/2
+2/2
+2/3
+3/4
+3/5
+0/1
+10/1
+9/10";
 
-//var input = smallInput;
-////var input = fullInput;
-////var input = smallest;
+var smallest = "";
 
-//var result = "";
+var input = smallInput;
+input = fullInput;
+//input = smallest;
+var timer = System.Diagnostics.Stopwatch.StartNew();
 
-//foreach (var line in input.Split(Environment.NewLine))
-//{
+var result = 0;
 
-//}
+var nodes = input.Split(Environment.NewLine).Select(x => x.Split("/").Select(int.Parse)).Select(x => new Node { In = x.First(), Out = x.Last() }).ToList();
 
-//Console.WriteLine(result);
-//Console.ReadLine();
+int Strength(Node n) => n.In + n.Out;
+int OpenConnection(Node n, int usedSide) => n.In == usedSide ? n.Out : n.In;
+
+var pq = new PriorityQueue<(Node current, HashSet<Node> unvisited, int openConnection, int strength), int>();
+foreach (var node in nodes.Where(x => x.In == 0 || x.Out == 0))
+{
+    pq.Enqueue((node, nodes.Where(x => x != node).ToHashSet(), OpenConnection(node, 0), Strength(node)), Strength(node));
+}
+
+
+while (pq.Count > 0)
+{
+    (Node current, HashSet<Node> unvisited, int openConnection, int strength) = pq.Dequeue();
+
+    result = Math.Max(result, strength);
+
+    foreach (var item in unvisited.Where(x => x.In == openConnection || x.Out == openConnection))
+    {
+        var newStrength = strength + Strength(item);
+        pq.Enqueue((item, unvisited.Where(x => x != item).ToHashSet(), OpenConnection(item, openConnection), newStrength), newStrength);
+    }
+
+}
+timer.Stop();
+Console.WriteLine(result);
+Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+Console.ReadLine();
+
+class Node
+{
+    public int In { get; set; }
+    public int Out { get; set; }
+    public override string ToString() => $"{In}/{Out}";
+}
